155802183c420cd95fa7aa41297786f709d02253
Дмитрий Шибаев <dima.shibaev2004@gmail.com>
2025-07-21 12:45:47 +0300

    Исправления

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -125,7 +125,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.'
+        'PageNumberPagination'
+    ),
+
     'PAGE_SIZE': 10,
 
 }
